0c934f8056277bd72dd2f0a42a32892c056dd6cb
rock12231 <37548802+rock12231@users.noreply.github.com>
2022-07-10 10:23:42 +0530

    pop and maker mouse position commented

--- Maps/views.py
+++ Maps/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Coordenadas
 from .forms import *
 import folium
+from folium.plugins import MousePosition
 
 def coordinates_form(request):
     coordinates = Coordenadas.objects.all()
@@ -20,8 +21,15 @@ def coordinates_form(request):
 
 
 def maps(request): 
+   
+    coordenadas = list(Coordenadas.objects.values_list('lat','lon'))[-1]
+    map = folium.Map(coordenadas)
+    folium.Marker(coordenadas).add_to(map)
+    folium.raster_layers.TileLayer('Stamen Terrain').add_to(map)
+    folium.raster_layers.TileLayer('Stamen Toner').add_to(map)
+    folium.raster_layers.TileLayer('Stamen Watercolor').add_to(map)
     
-       folium.Circle(
+    folium.Circle(
         radius=100,
         location=[26.463437, 80.296537],
         popup="The Waterfront",
@@ -37,16 +45,52 @@ def maps(request):
         fill=True,
         fill_color="#3186cc",
     ).add_to(map)
+
+
+    folium.GeoJson(
+        states[['STATE_NAME', 'geometry']].to_json(),
+        name='States',
+        show=True,
+        style_function=lambda x: {
+            'fillColor': 'lightblue',
+            'color': 'black',
+            'weight': 1,
+            'fillOpacity':0.7
+        },
+        highlight_function=lambda x: {
+            'fillOpacity':1
+        },
+        tooltip=folium.features.GeoJsonTooltip(
+            fields=['STATE_NAME'],
+            aliases=['State name:'],
+        ),
+    ).add_to(map)
     
-       
-    coordenadas = list(Coordenadas.objects.values_list('lat','lon'))[-1]
-    map = folium.Map(coordenadas)
-    folium.Marker(coordenadas).add_to(map)
-    folium.raster_layers.TileLayer('Stamen Terrain').add_to(map)
-    folium.raster_layers.TileLayer('Stamen Toner').add_to(map)
-    folium.raster_layers.TileLayer('Stamen Watercolor').add_to(map)
+    # pp= folium.Html('<a href="'+ 'give your url here'+'"target="_blank">'+ 'popup text' + '</a>', script=True)
+    # popup = folium.Popup(pp, max_width=2650)
+    # current = folium.LatLngPopup()
+    # folium.Marker(location=[26.420491, 80.314232], popup=popup).add_to(map)
     
- 
+    # folium.Marker(
+    #     popup = f'<input type="text" value="{location[0]}, {location[1]}" id="myInput"><button onclick="myFunction()">Copy location</button>'
+    # ).add_to(map)
+    
+    # folium.LatLngPopup().add_to(map)
+    # map.add_child(folium.LatLngPopup())
+    
+    # formatter = "function(num) {return L.Util.formatNum(num, 3) + ' º ';};"
+
+    # MousePosition(
+    #     position="topright",
+    #     separator=" | ",
+    #     empty_string="NaN",
+    #     lng_first=True,
+    #     num_digits=20,
+    #     prefix="Coordinates:",
+    #     lat_formatter=formatter,
+    #     lng_formatter=formatter,
+    # ).add_to(map)
+
     
     folium.LayerControl().add_to(map)
     map = map._repr_html_()
